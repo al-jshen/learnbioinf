b85384cc630174ba4ccd2a641f820a80e710e1af
Jeff Shen <22137276+al-jshen@users.noreply.github.com>
2020-03-24 14:54:30 -0400

    pattern count program tested

--- 1/wk1/PatternCount.py
+++ 1/wk1/PatternCount.py
@@ -1,9 +1,11 @@
-with open('./PatternCount.txt', 'r') as f:
+with open('./dataset_2_7.txt', 'r') as f:
     dat = f.read().split('\n')
-txt = dat[1]
-pat = dat[2]
+txt = dat[0]
+pat = dat[1]
 count = 0
 for i in range(len(txt) - len(pat)):
-    if txt[i:i+len(pat)] == pat:
+    windpat = txt[i:i+len(pat)]
+    print(windpat)
+    if windpat == pat:
         count += 1
 print(count)
